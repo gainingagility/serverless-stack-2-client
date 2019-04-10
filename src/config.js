76ee5f5ad90f2f528cd8766d495b645422d54603
The Ginger Prince <matthew.carr@gainingagility.com>
2019-04-10 13:50:41 +0100

    Adding Stripe keys to config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const dev = {
+  STRIPE_KEY: "YOUR_STRIPE_DEV_PUBLIC_KEY",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-dev-attachmentsbucket-lzf3amqy3ujk"
@@ -13,9 +14,10 @@ const dev = {
     APP_CLIENT_ID: "60c60062gbl1bilvsuq5as23j",
     IDENTITY_POOL_ID: "us-east-1:8cc4551c-3db1-412d-95d3-bc7f877d299a"
   }
-};
+  };
 
 const prod = {
+  STRIPE_KEY: "YOUR_STRIPE_PROD_PUBLIC_KEY",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-prod-attachmentsbucket-znmmkiitfcl5"
